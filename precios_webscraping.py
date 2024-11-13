5c975bddfbebeb4674ce5de9c2cb5f2767c45d66
fsoria <fersoria02@gmail.com>
2024-11-13 07:49:47 -0300

    Scraping de nuevos productos

--- precios_webscraping.py
+++ precios_webscraping.py
@@ -3,11 +3,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup as bs
-import time
 import psycopg2
+import time
 from datetime import datetime
 from dotenv import load_dotenv
 import os
+
 
 # Cargar las variables de entorno desde .env
 load_dotenv()
@@ -23,14 +24,19 @@ db_config = {
 
 def scrape_webpage(url):
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    driver_path = 'C:/Users/ferso/OneDrive/Escritorio/Proyectos/Precios supermercados/chromedriver.exe'
+    chrome_options.add_argument("--headless")  # Ejecuta en modo sin cabeza (sin abrir ventana del navegador)
+    driver_path = 'C:/Users/ferso/OneDrive/Escritorio/Proyectos/Precios supermercados/chromedriver.exe' 
     service = Service(driver_path)
     driver = webdriver.Chrome(service=service, options=chrome_options)
+    chrome_options.add_argument("--disable-logging")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--log-level=3")  # Solo errores
+
     
     driver.get(url)
     last_height = driver.execute_script("return document.body.scrollHeight")
     
+    # Realiza scroll hasta que ya no haya más carga dinámica
     while True:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(2)
@@ -44,41 +50,50 @@ def scrape_webpage(url):
     driver.quit()
     return page_source
 
-url = 'https://diaonline.supermercadosdia.com.ar/leche?_q=leche&map=ft'
-response = scrape_webpage(url)
-soup = bs(response, 'html.parser')
+# Lista de categorías con sus respectivas URLs y clases de HTML
+categorias = [
+    {'url': 'https://diaonline.supermercadosdia.com.ar/leche?_q=leche&map=ft', 
+     'nombre_clase': 'vtex-product-summary-2-x-productBrand vtex-product-summary-2-x-brandName t-body',
+     'precio_clase': 'vtex-product-price-1-x-sellingPriceValue'},
+    
+    {'url': 'https://diaonline.supermercadosdia.com.ar/yerba?_q=yerba&map=ft', 
+     'nombre_clase': 'vtex-product-summary-2-x-productBrand vtex-product-summary-2-x-brandName t-body',  
+     'precio_clase': 'vtex-product-price-1-x-sellingPrice'},  
+    
+    {'url': 'https://diaonline.supermercadosdia.com.ar/arroz?_q=arroz&map=ft', 
+     'nombre_clase': 'vtex-product-summary-2-x-productBrand vtex-product-summary-2-x-brandName t-body',  
+     'precio_clase': 'vtex-product-price-1-x-sellingPrice vtex-product-price-1-x-sellingPrice--hasListPrice'}, 
 
-productos = []
-productos_precio = []
-
-listado_nombres = soup.find_all('span', class_='vtex-product-summary-2-x-productBrand vtex-product-summary-2-x-brandName t-body')
-listado_precios = soup.find_all('span', class_='vtex-product-price-1-x-sellingPriceValue')
-
-for nombre in listado_nombres:
-    productos.append(nombre.text.strip())
-
-for precio in listado_precios:
-    if precio.find_previous('span', class_='vtex-product-summary-2-x-productBrand vtex-product-summary-2-x-brandName t-body'):
-        productos_precio.append(precio.text.strip())
+    {'url': 'https://diaonline.supermercadosdia.com.ar/panales?_q=panales&map=ft', 
+     'nombre_clase': 'vtex-product-summary-2-x-productBrand vtex-product-summary-2-x-brandName t-body',  
+     'precio_clase': 'vtex-product-price-1-x-sellingPrice'},  
+]
 
 productos_combinados = []
-palabras_a_omitir = ['crema', 'postre', 'condensada']
+palabras_a_omitir = ['crema', 'postre', 'condensada', 'fermentada', 'chocolatada', 'toallita', 'ropa', 'membrillo' ]
 fecha_scraping = datetime.now().strftime('%Y-%m-%d')
 
-for nombre, precio in zip(productos, productos_precio):
-    if not any(palabra in nombre.lower() for palabra in palabras_a_omitir):
-        productos_combinados.append({'Nombre': nombre, 'Precio': precio, 'Fecha_Scraping': fecha_scraping})
+for categoria in categorias:
+    response = scrape_webpage(categoria['url'])
+    soup = bs(response, 'html.parser')
+
+    listado_nombres = soup.find_all('span', class_=categoria['nombre_clase'])
+    listado_precios = soup.find_all('span', class_=categoria['precio_clase'])
+
+    productos = [nombre.text.strip() for nombre in listado_nombres]
+    productos_precio = [precio.text.strip() for precio in listado_precios if precio.find_previous('span', class_=categoria['nombre_clase'])]
+
+    for nombre, precio in zip(productos, productos_precio):
+        if not any(palabra in nombre.lower() for palabra in palabras_a_omitir):
+            productos_combinados.append({'Nombre': nombre, 'Precio': precio, 'Fecha_Scraping': fecha_scraping})
 
 df_combinado = pd.DataFrame(productos_combinados)
 
-# Guardar datos en PostgreSQL
 def save_to_postgresql(dataframe, db_config):
     try:
-        # Conectar a la base de datos
         conn = psycopg2.connect(**db_config)
         cursor = conn.cursor()
-        
-        # Crear la tabla si no existe
+
         create_table_query = """
         CREATE TABLE IF NOT EXISTS productos (
             id SERIAL PRIMARY KEY,
@@ -89,7 +104,6 @@ def save_to_postgresql(dataframe, db_config):
         """
         cursor.execute(create_table_query)
         
-        # Insertar datos en la tabla
         for _, row in dataframe.iterrows():
             insert_query = """
             INSERT INTO productos (nombre, precio, fecha_scraping) 
@@ -97,7 +111,6 @@ def save_to_postgresql(dataframe, db_config):
             """
             cursor.execute(insert_query, (row['Nombre'], row['Precio'], row['Fecha_Scraping']))
         
-        # Guardar cambios
         conn.commit()
         print("Datos guardados en PostgreSQL correctamente.")
         
@@ -107,5 +120,4 @@ def save_to_postgresql(dataframe, db_config):
         cursor.close()
         conn.close()
 
-# Llamada a la función para guardar datos en RDS
 save_to_postgresql(df_combinado, db_config)
--- selenium_script.py
+++ selenium_script.py
@@ -1,19 +1,56 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 def scrape_webpage(url):
     # Configuración del servicio para el controlador de Chrome
     service = Service(ChromeDriverManager().install())
-    
-    # Iniciar el controlador de Chrome
     driver = webdriver.Chrome(service=service)
-
-    # Navegar a la URL
     driver.get(url)
 
-    # Obtener el contenido de la página
-    page_source = driver.page_source  # Este es el contenido HTML
+    # Configuración de espera
+    wait = WebDriverWait(driver, 15)
+    product_locator = (By.CSS_SELECTOR, ".vtex-product-summary-2-x-productBrand.vtex-product-summary-2-x-brandName.t-body")
 
-    driver.quit()  # Cierra el navegador
-    return page_source  # Retorna el contenido
+    max_attempts = 30
+    attempts = 0
+    last_count = 0
+
+    while attempts < max_attempts:
+        # Intentar hacer clic en el botón "Cargar más" si está presente
+        try:
+            load_more_button = wait.until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, ".vtex-button__label.flex.items-center.justify-center.h-100.ph5"))
+            )
+            load_more_button.click()
+            time.sleep(3)  # Espera después de hacer clic en el botón
+        except:
+            # Si no hay botón de "Cargar más", continúa haciendo scroll
+            pass
+
+        # Scroll hasta el final de la página para activar carga dinámica
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        
+        # Espera para permitir que los productos carguen
+        time.sleep(5)
+
+        # Contar los productos actualmente visibles en la página
+        products = driver.find_elements(*product_locator)
+        current_count = len(products)
+
+        # Si no se agregan más productos, incrementa el contador de intentos
+        if current_count == last_count:
+            attempts += 1
+        else:
+            # Restablecer intentos si se detectan más productos
+            attempts = 0
+            last_count = current_count
+
+    # Obtener el contenido de la página después de todos los intentos de carga
+    page_source = driver.page_source
+    driver.quit()
+    return page_source 
